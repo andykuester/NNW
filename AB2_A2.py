6dd41dea95a3855fd16f0d1add8d5c61ff3671ff
andykuester <andreas.kuester@outlook.de>
2016-10-19 11:54:56 +0200

    AB2_A2 fast fertig

--- AB2_A2.py
+++ AB2_A2.py
@@ -21,15 +21,18 @@ class SLN:
     _cOut=0
     def __init__(self,dIn,cOut):
         self._b=np.zeros(cOut)
+        self._b=self._b[np.newaxis].T
+        
         np.random.seed(42)
         self._W=np.random.randn(dIn,cOut)/m.sqrt(dIn+1)
+        
         
     def neuron(self,X):
         net=np.zeros(X.shape[1]) 
   
-        net=self._W.dot(X)+self._b
+        net=self._W.dot(X) +self._b
              
-        return net >= self.threshold
+        return net >= self._threshold
         
                 
 D=np.loadtxt('iris.csv','float','#',',')
@@ -38,9 +41,9 @@ T=DT[4,:]
 X=DT[0:4,:]
                    
         
-nnw=SLN(1,X[0:2].shape[0]) # 
-result=nnw.neuron(X)
+nnw=SLN(1,X[0:2].shape[0]) # 1,2
+result=nnw.neuron(X[0:2])
 
-#nnwplot.plotTwoFeatures(X[0:2],T,)
+nnwplot.plotTwoFeatures(X[0:2],T,result)
         
     
