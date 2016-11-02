fd2339c3d230b65a02f24f87dad398b2d4e97a28
andykuester <andreas.kuester@outlook.de>
2016-11-02 11:54:09 +0100

    akshfkjd

--- AB2_A2.py
+++ AB2_A2.py
@@ -11,39 +11,67 @@ import numpy as np
 import math as m
 import nnwplot
 
-
+def ErrorRate(self,Y,T):
+    if  Y.ndim==1 or Y.shape[0]==1:
+        errors=Y!=T
+        return errors.sum()/Y.size
+    else: #für mehrere Ausgaben in one-hot Kodierung:
+        errors=Y.argmax(0)!=T.argmax(0)
+        return errors.sum()/Y.shape[1]
 
 
 
 class SLN:
-    _threshold=2
+    _threshold=0
     _dIn=0
     _cOut=0
+    _W=0
     def __init__(self,dIn,cOut):
-        self._b=np.zeros(cOut)
-        self._b=self._b[np.newaxis].T
-        
+        self._b=np.zeros(cOut)[np.newaxis].T
         np.random.seed(42)
-        self._W=np.random.randn(dIn,cOut)/m.sqrt(dIn+1)
-        
-        
+        self._W=np.random.randn(cOut,dIn)/np.sqrt(dIn)
     def neuron(self,X):
-        net=np.zeros(X.shape[1]) 
-  
-        net=self._W.dot(X) +self._b
-             
-        return net >= self._threshold
+        net=self._W.dot(X) +self._b        
+        return net >= self._threshold  
+    
         
-                
-D=np.loadtxt('iris.csv','float','#',',')
-DT=D.T
-T=DT[4,:]
-X=DT[0:4,:]
-                   
-        
-nnw=SLN(1,X[0:2].shape[0]) # 1,2
-result=nnw.neuron(X[0:2])
+    def DeltaTrain(self,X,T,eta,maxIter,maxErrorRate):
 
-nnwplot.plotTwoFeatures(X[0:2],T,result)
+        i = 0
+        err=1
+
+        N=X.shape[1]    # Anzahl Trainingsdaten    
+        x0 = np.ones(N)[np.newaxis]
         
+        while (i < maxIter and (err > maxErrorRate)):
+            
+            Y = self.neuron(X)
+            
+            #err = self.ErrorRate(y,T)
+            err = np.linalg.norm( T - y)
+            
+            self._W = self._W +( (eta*((T - y).dot(X.T))) / float(T.shape[0]))
+            self._b = self._b + ((T - y) / float(T.shape[0]))
+            
+            self._W+=eta*(T-Y).dot(X.T)/N
+            self._b+=eta*(T-Y).dot(x0.T)/N            
+            
+        
+       
+ 
+        
+iris = np.loadtxt("iris.csv",delimiter=',')
+X=iris[:,0:4].T
+T=iris[:,4]   
+                         
+        
+nnw=SLN(2,1) # 2,1
+nnw._W=np.array([-1,1])
+nnw._b=np.array([3])
+#result=nnw.neuron(X)
+eta=0.1
+maxIter=10
+maxErrorRate=0.01
+result=nnw.DeltaTrain(X,T,eta,maxIter,maxErrorRate)
+
     
