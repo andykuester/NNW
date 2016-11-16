7d5c891de0bf47205537da7ec5f2f86f425b0717
andykuester <andreas.kuester@outlook.de>
2016-11-16 11:15:22 +0100

    aufgabenblatt3

--- AB3_A1.py
+++ AB3_A1.py
@@ -6,7 +6,6 @@ Created on Wed Nov  9 09:40:10 2016
 """
 
 #%% AB 3
-
 import numpy as np
 import nnwplot
 import matplotlib.pyplot as plt
@@ -23,12 +22,27 @@ def ErrorRate(Y,T):
 #%%
 class SLN:
     def __init__(self,dIn,cOut): # Konstruktor
+        self._features=dIn
+        if dIn ==2:
+            self._print=True
+        else:
+            self._print=False
         self._b=np.zeros(cOut)[np.newaxis].T
         np.random.seed(42)
         self._W=np.random.randn(cOut,dIn)/np.sqrt(dIn)
-    def neuron(self,X):
-        net=self._W.dot(X)+self._b
-        return net>=0
+        if cOut==1:
+            self.neuron=self.threshold
+        else:
+            self.neuron=self.thresholdMult
+    def netsum(self,X):
+         return self._W.dot(X)+self._b        
+    def threshold(self,X):
+        return self.netsum(X)>=0
+    def onehot(self,T):
+        e=np.identity(self._W.shape[0])
+        return e[:,T.astype(int)]
+    def thresholdMult(self,X):
+        return self.onehot(self.netsum(X).argmax(0))
     def DeltaTrain(self, X, T, eta, maxIter, maxErrorRate):
         best = self;
         bestError = 2;
@@ -39,10 +53,10 @@ class SLN:
         for it in range(maxIter):
             Y = self.neuron(X)
             err = ErrorRate(Y, T)
-            if (it%20) == 0:
-                print('#{} {} {} {}'.format(it,self._W,self._b,err))
-                nnwplot.plotTwoFeatures(X,T,self.neuron)
-                plt.pause(0.05) # warte auf GUI event loop
+            #if (it%20) == 0:
+                #print('#{} {} {} {}'.format(it,self._W,self._b,err))
+                #nnwplot.plotTwoFeatures(X,T,self.neuron)
+                #plt.pause(0.05) # warte auf GUI event loop
             if err<bestError:
                 bestError = err
                 best = self
@@ -52,28 +66,69 @@ class SLN:
             self._W+=eta*(T-Y).dot(X.T)/N
             self._b+=eta*(T-Y).dot(x0.T)/N
         self=best
-        print('#{} {} {} {}'.format(it,self._W,self._b,err))
-        nnwplot.plotTwoFeatures(X,T,self.neuron)
-        plt.pause(0.05) # warte auf GUI event loop
+        if self._print:
+            #print('#{} {} {} {}'.format(it,self._W,self._b,err))
+            nnwplot.plotTwoFeatures(X,T,self.neuron)
+            plt.pause(0.05) # warte auf GUI event loop
+        else:
+            print('Training with {} features finished'.format(self._features))
+    
         return bestError, bestIt
+    def test(self,X):
+        r= slnIris.neuron(X)
+        iris_type=np.argmax(r)
+        print('Input:')
+        print('{} '.format(X))
+        print('Iris-Type: {}'.format(iris_type))
         
-    def onehot(self,T):
-        e=np.identity(self._W.shape[0])
-        return e[:,T.astype(int)]
-
 
 #%% Iris-Daten Laden
 iris = np.loadtxt("iris.csv",delimiter=',')
 X=iris[:,0:4].T
 T=iris[:,4]
 
-#%% Training mit 2 Iris-Blütenarten, Merkmale 0 und 1
+#%% oneHotTest
+TestT = np.array([0,2,1,2])
+slnTest = SLN(2,3)
+TestResult=slnTest.onehot(TestT)
+
+#%% Training mit 3 Iris-Blütenarten 
+# merkemale 1,2
+print('merkemale 1,2')
 plt.figure()
-slnIris = SLN(2,1)
-slnIris.DeltaTrain(X[:2,0:100],T[0:100],0.1,20000000,0.02)
-slnIris.neuron(X[:2,:])
+newX=X[:2,:]
+slnIris1 = SLN(2,3)
+oneHot1=slnIris1.onehot(T)
+slnIris1.DeltaTrain(newX,oneHot1,0.1,3000,0.01)
+
+# merkemale 2,3
+print('merkemale 2,3')
+plt.figure()
+newX=X[1:3,:]
+slnIris2 = SLN(2,3)
+oneHot2=slnIris2.onehot(T)
+slnIris2.DeltaTrain(newX,oneHot2,0.1,3000,0.01)
+
+# merkemale 3,4
+print('merkemale 3,4')
+plt.figure()
+newX=X[2:,:]
+slnIris3 = SLN(2,3)
+oneHot3=slnIris3.onehot(T)
+slnIris3.DeltaTrain(newX,oneHot3,0.1,3000,0.01)
 
 
+#%% Training mit 3 Iris-Blütenarten
+# alle merkmale
+plt.figure()
+newX=X[:,:]
+slnIris4 = SLN(4,3)
+oneHot4=slnIris4.onehot(T)
+slnIris4.DeltaTrain(newX,oneHot4,0.1,100000,0.01)
+finalResult=slnIris4.neuron(newX)
 
+#Trainiertes netz testen
+testX = np.array([[5.2],[1.2],[5.1],[3.4]])
+slnIris4.test(testX)
 
 
